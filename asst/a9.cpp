f643fb93acefaf02ee5c9357e4ebe62cfe4aa379
Trevor <trevorsc@mit.edu>
2021-11-29 21:56:25 -0500

    First commit, undecided idea.

---
+++ asst/a9.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+#include "a9.h"
+
+using namespace std;
+
+// Write your implementations here, or extend the Makefile if you add source
+// files
+void someFunction() {
+    cout << "ok, that's a function" << endl;
+}
---
+++ asst/a9.h
@@ -0,0 +1,10 @@
+#ifndef A9_H_PHUDVTKB
+#define A9_H_PHUDVTKB
+
+// Write your declarations here, or extend the Makefile if you add source
+// files
+void someFunction();
+
+
+#endif /* end of include guard: A9_H_PHUDVTKB */
+
