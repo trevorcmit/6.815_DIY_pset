b5add4ba7316e8f7cba2b31f1023538efa589711
Trevor <trevorsc@mit.edu>
2021-12-03 22:58:14 -0500

    Added horizontal seam and test case.

--- asst/a9.cpp
+++ asst/a9.cpp
@@ -96,46 +96,38 @@ vector<int> vert_minimum_seam(const Image &cme) {
     // Calculate the minimum seam of an input vertical cumulative energy map.
     // Input: One channel image representing vertical CME.
     // Output: Vector of integer indices representing row indices of minimum energy.
-    vector<int> output; // Initialize output indices vector
+    vector<int> output;      // Initialize output indices vector
     float min = 1000.0f;     // Initialize value for mininum energy
-    int index = 1000;   // Initailize variable for min. energy index
+    int index = 1000;        // Initailize variable for min. energy index
 
-    for (int x = 0; x < cme.width(); x++) {
+    for (int x = 0; x < cme.width(); x++) { // Iterate over bottom row to get starting index
         if (cme(x, cme.height()-1) < min) {
-            min = cme(x, cme.height()-1);
-            index = x;
+            min = cme(x, cme.height()-1);   // Store minimum for next iteration
+            index = x;                      // Set new starting index if energy less than existing
         }
     }
     output.push_back(index);
 
-    for (int y = 0; y <= cme.height() - 2; y++) {
+    for (int y = 0; y <= cme.height() - 2; y++) { // Iterate over all rows except bottom row
         int h = cme.height() - 2 - y;
 
         if (index == 0) { // Left edge case, two options for next index
-            float m = cme(0, h);
-            float r = cme(1, h);
-            if (m < r) {index = 0;}
-            else       {index = 1;}
+            if (cme(0, h) < cme(1, h)) {index = 0;}
+            else                       {index = 1;}
         }
-
         else if (index == cme.width()-1) { // Right edge case, two options for next index
-            float m = cme(cme.width()-1, h);
-            float l = cme(cme.width()-2, h);
-            if (m < l) {index = cme.width()-1;}
-            else       {index = cme.width()-2;}
+            if (cme(cme.width()-1, h) < cme(cme.width()-2, h)) {index = cme.width()-1;}
+            else                                               {index = cme.width()-2;}
         }
-
         else { // Middle case, three options for next index
             float l = cme(index-1, h);
             float m = cme(index, h);
             float r = cme(index+1, h);
-            if ((l < m) && (l < r)) {index = index - 1;}      // Left is new min index
-            else if ((r < m) && (r < m)) {index = index + 1;} // Right is new min index
+            if ((l < m) && (l < r))      {index = index - 1;} // Left is new min index
+            else if ((r < m) && (r < l)) {index = index + 1;} // Right is new min index
         }
-
         output.push_back(index);
     }
-
     return output;
 }
 
@@ -144,7 +136,38 @@ vector<int> hori_minimum_seam(const Image &cme) {
     // Calculate the minimum seam of an input horizontal cumulative energy map.
     // Input: One channel image representing horizontal CME.
     // Output: Vector of integer indices representing column indices of minimum energy.
-    vector<int> output;
+    vector<int> output;      // Initialize output indices vector
+    float min = 1000.0f;     // Initialize value for mininum energy
+    int index = 1000;        // Initailize variable for min. energy index
+
+    for (int y = 0; y < cme.height(); y++) { // Iterate over right column to get starting index
+        if (cme(cme.width()-1, y) < min) {
+            min = cme(cme.width()-1, y);     // Store minimum for next iteration
+            index = y;                       // Set new starting index if energy less than existing
+        }
+    }
+    output.push_back(index);
+
+    for (int x = 0; x <= cme.width() - 2; x++) {     // Iterate over all rows except bottom row
+        int w = cme.width() - 2 - x;
+
+        if (index == 0) {                            // Top edge case, two options for next index
+            if (cme(w, 0) < cme(w, 1)) {index = 0;}
+            else                       {index = 1;}
+        }
+        else if (index == cme.height()-1) {          // Bottom edge case, two options for next index
+            if (cme(w, cme.height()-1) < cme(w, cme.height()-2)) {index = cme.height()-1;}
+            else                                                 {index = cme.height()-2;}
+        }
+        else { // Middle case, three options for next index
+            float a = cme(w, index-1); // New index is above
+            float m = cme(w, index);   // New index is the same
+            float b = cme(w, index+1); // new index is below
+            if ((a < m) && (a < b))      {index = index - 1;} // Left is new min index
+            else if ((b < m) && (b < a)) {index = index + 1;} // Right is new min index
+        }
+        output.push_back(index);
+    }
     return output;
 }
 
--- asst/a9_main.cpp
+++ asst/a9_main.cpp
@@ -7,7 +7,6 @@
  *
  * ------------------------------------------------------------------------*/
 
-
 #include <iostream>
 #include "a9.h"
 #include "Image.h"
@@ -50,30 +49,52 @@ void test_hori_cme() { // Test vert_cumulative_energy_map function on two images
 
 
 void test_vert_seam() {
-  Image pigbird("./Input/pigbird.png"); // Load images
+  Image pigbird("./Input/pigbird.png");              // Load images
   Image twocats("./Input/twocats.png"); 
-  Image pbl1 = vert_cumulative_energy_map(pigbird); // Make CME's
+  Image pbl1 = vert_cumulative_energy_map(pigbird);  // Make CME's
   Image tcl1 = vert_cumulative_energy_map(twocats);
-
-  vector<int> pb_seam = vert_minimum_seam(pbl1); // Get seams
+  vector<int> pb_seam = vert_minimum_seam(pbl1);     // Get seams
   vector<int> tc_seam = vert_minimum_seam(tcl1);
 
-  for (int n = 0; n < pigbird.height(); n++) {
+  for (int n = 0; n < pigbird.height(); n++) { // Highlight seam on Pigbird
     int h = pigbird.height() - 1 - n;
     pigbird(pb_seam.at(h), h, 0) = 1.0f;
     pigbird(pb_seam.at(h), h, 1) = 0.0f;
     pigbird(pb_seam.at(h), h, 2) = 0.0f;
   }
-
-  for (int n = 0; n < twocats.height(); n++) {
+  for (int n = 0; n < twocats.height(); n++) { // Highlight seam on Twocats
     int h = twocats.height() - 1 - n;
     twocats(tc_seam.at(h), h, 0) = 1.0f;
     twocats(tc_seam.at(h), h, 1) = 0.0f;
     twocats(tc_seam.at(h), h, 2) = 0.0f;
   }
-
   pigbird.write("./Output/test_vert_seam_pigbird.png");
   twocats.write("./Output/test_vert_seam_twocats.png");
+}
+
+
+void test_hori_seam() {
+  Image pigbird("./Input/pigbird.png");              // Load images
+  Image twocats("./Input/twocats.png"); 
+  Image pbl1 = hori_cumulative_energy_map(pigbird);  // Make CME's
+  Image tcl1 = hori_cumulative_energy_map(twocats);
+  vector<int> pb_seam = hori_minimum_seam(pbl1);     // Get seams
+  vector<int> tc_seam = hori_minimum_seam(tcl1);
+
+  for (int n = 0; n < pigbird.width(); n++) { // Highlight seam on Pigbird
+    int w = pigbird.width() - 1 - n;
+    pigbird(w, pb_seam.at(w), 0) = 1.0f;
+    pigbird(w, pb_seam.at(w), 1) = 0.0f;
+    pigbird(w, pb_seam.at(w), 2) = 0.0f;
+  }
+  for (int n = 0; n < twocats.width(); n++) { // Highlight seam on Twocats
+    int w = twocats.width() - 1 - n;
+    twocats(w, tc_seam.at(w), 0) = 1.0f;
+    twocats(w, tc_seam.at(w), 1) = 0.0f;
+    twocats(w, tc_seam.at(w), 2) = 0.0f;
+  }
+  pigbird.write("./Output/test_hori_seam_pigbird.png");
+  twocats.write("./Output/test_hori_seam_twocats.png");
 }
 
 
@@ -84,6 +105,7 @@ int main() {
   test_vert_cme();
   test_hori_cme();
   test_vert_seam();
+  test_hori_seam();
 
   clock_t end = clock();
   double duration = (end - start) * 1.0f / CLOCKS_PER_SEC;
